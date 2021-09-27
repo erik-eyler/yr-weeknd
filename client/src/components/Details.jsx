633f18b468551e54b9fd2bc551f3129f06f6f68c
Erik Eyler <erikeyler@Eriks-MacBook-Air.local>
2021-09-26 21:56:37 -0400

    reworking card divs on Details component

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -16,27 +16,32 @@ export default function Details() {
   }, []);
 
   return (
-    <div className="bg-indigo-300 rounded-lg shadow-lg p-1">
-      <div className="flex flex-wrap w-1/3 mb-4" >
-        <img src={place?.fields?.image} className="rounded-lg shadow" alt="Yr Weeknd" />
+    <div className="bg-white w-screen h-screen flex flex-row flex-wrap p-1">
+      <div className="mx-auto w-2/3">
+        <div className="bg-indigo-300 rounded-lg shadow-lg w-full flex-row flex-wrap p-1">
+
+        <div className="flex flex-wrap md:w-1/3 w-full" >
+          <img src={place?.fields?.image} className="rounded-lg shadow" alt="Yr Weeknd" />
+        </div>
+        <div className="flex mb-4" >
+          <h4>Location: {place?.fields?.location}</h4>
+        </div>
+        <div className="flex flex-wrap mb-4" >
+          <h4>Activity: {place?.fields?.activity}</h4>
+        </div>
+        <div className="flex flex-wrap mb-4" >
+          <h4>Length: {place?.fields?.length}</h4>
+        </div>
+        <div className="flex flex-wrap mb-4" >
+          <h4>Rating: {place?.fields?.rating}</h4>
+        </div>
+        <div className="flex flex-wrap mb-4" >
+          <h4>Description: {place?.fields?.description}</h4>
+        </div>
+        <Link to={`/places/${place.id}/edit`}>Edit</Link>
+          <Delete id={place.id} />
+          </div>
       </div>
-      <div className="flex mb-4" >
-        <h4>Location: {place?.fields?.location}</h4>
-      </div>
-      <div className="flex flex-wrap mb-4" >
-        <h4>Activity: {place?.fields?.activity}</h4>
-      </div>
-      <div className="flex flex-wrap mb-4" >
-        <h4>Length: {place?.fields?.length}</h4>
-      </div>
-      <div className="flex flex-wrap mb-4" >
-        <h4>Rating: {place?.fields?.rating}</h4>
-      </div>
-      <div className="flex flex-wrap mb-4" >
-        <h4>Description: {place?.fields?.description}</h4>
-      </div>
-      <Link to={`/places/${place.id}/edit`}>Edit</Link>
-      <Delete id={place.id} />
     </div>
   )
 }

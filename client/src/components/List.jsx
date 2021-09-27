c37ef164bc44e96c8a6ab82633e0aaa24bcef8a1
Erik Eyler <erikeyler@Eriks-MacBook-Air.local>
2021-09-27 13:40:36 -0400

    fixed image padding on List component

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -18,7 +18,7 @@ export default function List() {
         return (
           <Link to={`/places/${place.id}`} key={place.id}>
             <h3>{place.fields.location}</h3>
-            <img src={place.fields.image} className="rounded-lg shadow" alt="Yr Weeknd" />
+            <img src={place.fields.image} className="w-2/3 h-2/3 mx-auto rounded-lg shadow" alt="Yr Weeknd" />
           </Link>
         );
       })}
